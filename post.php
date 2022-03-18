6533f3ba7162ec4aadb4764997f3e0cf1f3d98ab
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-03-18 21:36:36 +0100

    Fix post page, add a placeholder template.

--- post.php
+++ post.php
@@ -3,7 +3,7 @@ require('lib/common.php');
 
 $id = isset($_GET['id']) ? $_GET['id'] : null;
 
-$post = fetch("SELECT * FROM music WHERE id = ?", [$id]);
+$post = fetch("SELECT * FROM posts WHERE id = ?", [$id]);
 
 if (!$post) error('404', "Invalid post ID.");
 
